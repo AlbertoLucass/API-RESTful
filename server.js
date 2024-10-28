3a9f0f8b3b33015b14c47ba7a1d71aba42c1f62d
laizmesquita <laizmesquita@gmail.com>
2024-10-28 13:52:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.send('Servidor Express inicializado...');
 });
 
+app.post('/aluno', (req, res) => {
+  const {nome, email, nome_curso} = req.body;
+  const vaga = create ({nomw, email, nome_curso});
+  res.status(201).json(vaga);
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
