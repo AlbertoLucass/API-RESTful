c9b7a43feeb552cbcfc73360e176699f6297b7df
laizmesquita <laizmesquita@gmail.com>
2024-10-28 13:49:31 -0300

    Update aluno.js

--- repositories/aluno.js
+++ repositories/aluno.js
@@ -13,32 +13,6 @@ function create ({nome, email, nome_curso}){
     return aluno;
 }
 
-function update(id, {nome, email, nome_curso}){
-    const index = vahas.findIndex(aluno => aluno.id === id);
-    if (index === -1){
-        return null;
-    }
-    vagas [index] = {
-        is,
-        nome,
-        email,
-        nome_curso,
-    };
-    return aluno[index];
-
-    function remove (id) {
-        const index = aluno.findIndex(aluno => aluno.id === id);
-        if (index === -1) {
-            return false;
-        }
-        aluno.splice(index, 1);
-        return true;
-    }
-
-    function findAll() {
-        return aluno;
-    }
-
     module.exports = {
         create,
         update,
