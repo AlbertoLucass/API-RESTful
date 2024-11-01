ebb7e58fc445da00e8b85cc45f7c4120c9edd41f
Hugo Gabriel <hugo.gabriel@outlook.com.br>
2024-11-01 10:41:51 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const { create,update,remove,findAll } = require('./repositories/aluno');
+const express = require("express");
+const { create, update, remove, findAll } = require("./repositories/aluno");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Servidor Express inicializado...');
+app.get("/", (req, res) => {
+  res.send("Servidor Express inicializado...");
 });
 
-app.post('/aluno', (req, res) => {
-    const {nome, email, nome_curso} = req.body;
-    const aluno = create({nome, email, nome_curso});
-    res.status(201).json(aluno);
+app.post("/aluno", (req, res) => {
+  const { nome, email, nome_curso } = req.body;
+  const aluno = create({ nome, email, nome_curso });
+  res.status(201).json(aluno);
 });
 
-
-
-app.put('/aluno/:id', (req, res) => {
-    const { id } = req.params;
-    const {nome, email, nome_curso} = req.body;
-    const aluno = update(id, {nome, email, nome_curso});
-    res.json(aluno);
+app.put("/aluno/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, email, nome_curso } = req.body;
+  const aluno = update(id, { nome, email, nome_curso });
+  res.json(aluno);
 });
 
-
+app.delete("/aluno/:id", (req, res) => {
+  const { id } = req.params;
+  remove(id);
+  res.status(204).send();
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
